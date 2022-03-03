2d6cb3f9f40fb3e8690c1335460811d5013e8e3c
rohit0805 <rohit5822540@gmail.com>
2022-03-03 18:43:14 +0530

    react init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = ()=>{
+  return (
+    <div>
+      MY APP
+    </div>
+  );
+}
+
+export default App; 
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(<h1>App</h1>,document.querySelector("#root"));
